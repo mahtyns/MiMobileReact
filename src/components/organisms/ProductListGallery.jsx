ba017242d9154254f1573699e48369b6c2176f6d
Martyna <m.smolarek19@gmail.com>
2022-12-05 08:05:13 +0100

    sorting function

--- src/components/organisms/ProductListGallery.jsx
+++ src/components/organisms/ProductListGallery.jsx
@@ -8,17 +8,29 @@ import { Link } from "react-router-dom";
 const ProductListGallery = ({ productList, searchItem }) => {
 
   return (
+    // <div className='productGridContainer'>
+    //   {productList.map(product => 
+    //     <Link key={product.product_id} to={`/products/${product.product_id}`}>
+    //     <ProductItemListed 
+    //       key={product.product_id} 
+    //       productName={product.product_brand + ' ' + product.product_model} 
+    //       imageAlt={product.product_brand + ' ' + product.product_model} 
+    //       imageSrc={product.product_img} 
+    //       productPrice={product.product_price}/>
+    //     </Link>)
+    //     }
+    // </div>
     <div className='productGridContainer'>
-      {productList.map(product => 
-        <Link key={product.product_id} to={`/products/${product.product_id}`}>
-        <ProductItemListed 
-          key={product.product_id} 
-          productName={product.product_brand + ' ' + product.product_model} 
-          imageAlt={product.product_brand + ' ' + product.product_model} 
-          imageSrc={product.product_img} 
-          productPrice={product.product_price}/>
-        </Link>)
-        }
+      {
+        productList.filter(product => {
+          if (searchItem === '') return <ProductItemListed key={product.product_id} productPrice={product.product_price} imageSrc={product.product_img}  productName={product.product_brand + ' ' + product.product_model} />
+          else if (product.product_brand.toLowerCase().includes(searchItem.toLowerCase())) return <ProductItemListed key={product.product_id} productPrice={product.product_price} imageSrc={product.product_img} productName={product.product_brand + ' ' + product.product_model} />
+          else if (product.product_model.toLowerCase().includes(searchItem.toLowerCase())) return <ProductItemListed key={product.product_id} productPrice={product.product_price} imageSrc={product.product_img} productName={product.product_brand + ' ' + product.product_model} />
+        }).map(product => <Link key={product.product_id} to={`/products/${product.product_id}`}>
+        <ProductItemListed key={product.product_id} productPrice={product.product_price} imageSrc={product.product_img} productName={product.product_brand + ' ' + product.product_model} />
+        </Link>
+        )
+      }
     </div>
   )
 }
